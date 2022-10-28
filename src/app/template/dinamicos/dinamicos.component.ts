591a3ba00a1d637c04f5dd61bdb5fb9148fc9c60
PC <fernandezfrancoa2001@gmail.com>
2022-10-28 00:50:34 -0300

    forms template

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,16 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Persona{
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito{
+  id: number;
+  nombre: string;
+}
+
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent  {
 
-  constructor() { }
+  nuevoJuego: string = "";
 
-  ngOnInit(): void {
+  persona: Persona = {
+    nombre: "Franco",
+    favoritos: [
+      {id: 1, nombre: "GTA V"},
+      {id: 2, nombre: "Plantas vs Zombies"}
+    ]
+
+  }
+
+  guardar(){
+
+  }
+
+  eliminar(index: number){
+    this.persona.favoritos.splice(index, 1)
+  }
+
+  agregarJuego(){
+    const nuevoFavorito: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+    }
+
+    this.persona.favoritos.push({...nuevoFavorito});
+    this.nuevoJuego = "";
   }
 
 }
